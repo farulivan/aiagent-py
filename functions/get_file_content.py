600c64204511f070c9166e567cbafc2b6d4365cd
farulivan <farul@nabitu.id>
2026-03-03 14:26:17 +0700

    feat: add file content reader with truncation and path validation 📄

---
+++ functions/get_file_content.py
@@ -0,0 +1,25 @@
+import os
+
+from config import MAX_CHARS
+
+
+def get_file_content(working_directory, file_path):
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
+        file_path_abs = os.path.normpath(os.path.join(working_dir_abs, file_path))
+        valid_file_path_abs = os.path.commonpath([working_dir_abs, file_path_abs]) == working_dir_abs
+
+        if not valid_file_path_abs:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        if not os.path.isfile(file_path_abs):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        
+        with open(file_path_abs, "r") as f:
+            content = f.read(MAX_CHARS)
+            if f.read(1):
+                content += f'\n[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        return content
+        
+    except Exception as e:
+        return f'Error reading file "{file_path}": {e}'
